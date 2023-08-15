2c447d23694c6b86ce6343c925fd5df842f3aa6d
Ryomax17 <42577059+Ryomax17@users.noreply.github.com>
2023-08-14 21:48:34 -0300

    titulo al seleccionar una opcion

--- src/api/init.js
+++ src/api/init.js
@@ -1,56 +1,58 @@
 console.log("El archivo init.js se ha cargado correctamente.");
 
-//dirección para obtener el listado en formato json:
-const pokeapi_URL = "https://pokeapi.co/api/v2/";
-const ability_URL = "https://pokeapi.co/api/v2/ability/";
-const pokemon_URL = "https://pokeapi.co/api/v2/pokemon/";
-const region_URL = "https://pokeapi.co/api/v2/region/";
-
-function getJSONData(url) {
-  return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw Error(response.statusText);
-      }
-    })
-    .then(data => {
-      return { status: 'ok', data: data };
-    })
-    .catch(error => {
-      return { status: 'error', data: error };
-    });
-}
-
-function displayJSONData(url) {
-  getJSONData(url)
-    .then(data => {
-      const list = document.getElementById("dataList"); // Obtener el elemento de lista
-      list.innerHTML = ""; // Limpiar contenido anterior
-
-      // Iterar sobre los datos y agregar elementos de lista
-      data.data.results.forEach(item => {
-        const listItem = document.createElement("li");
-        listItem.textContent = `${item.name}`;
-        list.appendChild(listItem);
-      });
-    })
-    .catch(error => {
-      console.error("Error al obtener datos JSON:", error);
-    });
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-  const searchButton = document.getElementById("searchButton"); // Obtener el botón "Search"
-  const selectCategory = document.querySelector(".form-select"); // Obtener el select
+  const categorySelect = document.getElementById("categorySelect");
+  const searchButton = document.getElementById("searchButton");
+  const dataList = document.getElementById("dataList");
+  const selectedCategoryTitle = document.getElementById("selectedCategoryTitle");
+  const selectedCategorySection = document.getElementById("selectedCategorySection");
 
-  // Agregar evento de clic al botón "Search"
+  const pokeapi_URL = "https://pokeapi.co/api/v2/";
+  const ability_URL = "https://pokeapi.co/api/v2/ability/";
+  const pokemon_URL = "https://pokeapi.co/api/v2/pokemon/";
+  const region_URL = "https://pokeapi.co/api/v2/region/";
+
+  function getJSONData(url) {
+    return fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+      })
+      .then(data => {
+        return { status: 'ok', data: data };
+      })
+      .catch(error => {
+        return { status: 'error', data: error };
+      });
+  }
+
+  function displayJSONData(url) {
+    getJSONData(url)
+      .then(data => {
+        dataList.innerHTML = ""; // Limpiar contenido anterior
+
+        // Mostrar el título de la categoría seleccionada
+        const selectedCategory = categorySelect.options[categorySelect.selectedIndex].text;
+        selectedCategoryTitle.textContent = selectedCategory;
+        selectedCategorySection.classList.remove("d-none"); // Mostrar la sección del título
+
+        // Iterar sobre los datos y agregar elementos de lista
+        data.data.results.forEach(item => {
+          const listItem = document.createElement("li");
+          listItem.textContent = `${item.name}`;
+          dataList.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        console.error("Error al obtener datos JSON:", error);
+      });
+  }
+
   searchButton.addEventListener("click", function () {
-    // Obtener el valor seleccionado del select
-    const selectedValue = selectCategory.value;
-
-    // Según la opción seleccionada, obtener la URL correspondiente
+    const selectedValue = categorySelect.value;
     let categoryURL = "";
     if (selectedValue === "1") {
       categoryURL = pokemon_URL;
@@ -59,8 +61,6 @@ document.addEventListener("DOMContentLoaded", function () {
     } else if (selectedValue === "3") {
       categoryURL = region_URL;
     }
-
-    // Llamar a la función para mostrar los datos
     displayJSONData(categoryURL);
   });
 
